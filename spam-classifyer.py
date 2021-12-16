c5350f38a62afaa81a7f6189aec2f37ff19f69da
Christoffer Willander <christoffer.willander@gmail.com>
2021-12-16 16:59:58 +0100

    Initial implemenation

--- spam-classifyer.py
+++ spam-classifyer.py
@@ -23,16 +23,18 @@ def main():
     #scaler = StandardScaler()
     #dataX = scaler.fit_transform(dataX)
 
+    # Initializing array for classification models
     models = []
+
+    # Creating SVC, Gaussian Naive Bayes, Random Forest classification models
     models.append(svm.SVC())
     models.append(GaussianNB())
     models.append(RandomForestClassifier())
 
+    # Initializing arrays for storing model metrics
     modelAccuracies = []
     modelF1Scores = []
     modelTrainingTimes = []
-
-    curFold = 1
 
     # Performing stratified 10-fold cross validation, training and evaluation
     for trainIndex, testIndex in stratKFold.split(dataX, dataY):
@@ -43,7 +45,7 @@ def main():
         curModelF1Scores = []
         curModelTrainingTimes = []
         for model in range(len(models)):
-            accuracy, f1Score, trainingTime = trainModel(models[model], trainFold, testFold, curFold)
+            accuracy, f1Score, trainingTime = trainModel(models[model], trainFold, testFold)
             curModelAccuracies.append(accuracy)
             curModelF1Scores.append(f1Score)
             curModelTrainingTimes.append(trainingTime)
@@ -51,14 +53,13 @@ def main():
         modelAccuracies.append(np.array(curModelAccuracies))
         modelF1Scores.append(np.array(curModelF1Scores))
         modelTrainingTimes.append(np.array(curModelTrainingTimes))
-        curFold += 1
     
     printResults(modelAccuracies, models, "ACCURACY", False)
     printResults(modelF1Scores, models, "F1 SCORE", False)
     printResults(modelTrainingTimes, models, "TRAINING TIME", True)
 
 
-def trainModel(model, trainFold, testFold, foldNum):
+def trainModel(model, trainFold, testFold):
     trainX = trainFold[trainFold.columns[0:57]]
     trainY = trainFold[57]
 
@@ -76,27 +77,24 @@ def trainModel(model, trainFold, testFold, foldNum):
 def printResults(data, models, statType, ascOrder):
     headers = ["Fold"]
     dataRows = []
+    meanRank = [0, 0, 0]
 
     for model in range(len(models)):
         headers.append(type(models[model]).__name__)
 
     for dataPoints in range (len(data)):
         dataRow = [str(dataPoints + 1)]
-        #dataRanks = np.argsort(data[dataPoints])
         dataRanks = pd.DataFrame(data[dataPoints])
         dataRanks = dataRanks.rank(ascending=ascOrder)
         dataRanks = dataRanks.to_numpy()
-        #print(ranks.rank(ascending=ascOrder))
-
-        # if (ascOrder):
-        #     dataRanks = dataRanks[::-1][:len(dataRanks)]
 
         for dataPoint in range(len(data[dataPoints])):
             curValue = format(round(data[dataPoints][dataPoint], 4), '.4f')
             tempRow = str(curValue) + " (" + str(int(dataRanks[dataPoint])) + ")"
             dataRow.append(tempRow)
+            meanRank[dataPoint] += int(dataRanks[dataPoint])
         dataRows.append(dataRow)
-
+    
     tempRow = ["MEAN: "]
     for dataPoints in range(len(data[0])):
         tempValues = []
@@ -104,6 +102,24 @@ def printResults(data, models, statType, ascOrder):
             tempValues.append(data[dataPoint][dataPoints])
         meanVal = sum(tempValues) / len(tempValues)
         tempRow.append(format(round(meanVal, 4), '.4f'))
+    
+    dataRows.append(tempRow)
+
+    tempRow = ["MEAN RANK: "]
+    for model in range(len(meanRank)):
+        meanRank[model] = meanRank[model]/len(data)
+        tempRow.append(format(round(meanRank[model], 4), '.2f'))
+
+    dataRows.append(tempRow)
+    tempRow = ["S.D.: "]
+
+    for dataPoints in range(len(data[0])):
+        tempValues = []
+        for dataPoint in range(len(data)):
+            tempValues.append(data[dataPoint][dataPoints])
+        tempValues = np.array(tempValues)
+        sdVal = np.std(tempValues)
+        tempRow.append(format(round(sdVal, 4), '.4f'))
     
     dataRows.append(tempRow)
 
